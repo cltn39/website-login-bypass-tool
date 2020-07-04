4eafc2db019d67f22c006850f2cd68c90cd417fd
Chisoo Song <cltn39@gmail.com>
2020-07-04 12:03:33 -0700

    working bypass login

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
+import hashlib
+from pprint import pprint
+
 import requests
-import pprint
+from bs4 import BeautifulSoup
 
 from config import username, password, targetUrl
 
+def get_md5(s):
+    return hashlib.md5(bytes(s, encoding='utf8')).hexdigest()
 
 def main():
     url = targetUrl
     
     with requests.session() as session:
-        response = session.post(url, auth=(username, password))
+        response = session.get(url)
         
-        pprint(response.text)
+        md5_pass = get_md5(password) + ':'
+        email = username + ':'
+
+        soup = BeautifulSoup(response.text, 'lxml')
+        challenge = soup.find('input', id='challange').get('value')
+
+        result = email + md5_pass + challenge
+        response = get_md5(result)
+
+        data = {'username': username,
+                'password': '',
+                'challenge': '',
+                'response': response
+        }
+
+        r_post = session.post(url, data=data)
+        pprint(r_post.text)
+
 
         with open('index.html', 'w') as f:
-            f.write(response.text)
+            f.write(r_post.text)
 
 
 
